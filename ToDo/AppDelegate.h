92503065135a88990416dfdebbd7d66f5849bfd7
Vasanthy Kolluri <vasanthykolluri@gmail.com>
2014-01-18 18:08:36 -0800

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Vasanthy Kolluri on 1/18/14.
+//  Copyright (c) 2014 Vasanthy Kolluri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDo
+//
+//  Created by Vasanthy Kolluri on 1/18/14.
+//  Copyright (c) 2014 Vasanthy Kolluri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
